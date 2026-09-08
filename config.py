d0d88c849a069314cf83cedc6b45b6219c31d30b
itsakermanlevi728292-blip <itsakermanlevi728292@gmail.com>
2026-09-08 23:53:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ragini19854-prog/light_alisa-bot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itsakermanlevi728292-blip/Alisamusicbot.git")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
