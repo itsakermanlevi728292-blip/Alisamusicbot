4df969dbba69e0ddab763f38a2e1a64a7e8fc93b
itsakermanlevi728292-blip <itsakermanlevi728292@gmail.com>
2026-09-14 18:25:12 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -110,7 +110,6 @@ class YouTubeAPI:
         self.status = "https://www.youtube.com/oembed?url="
         self.listbase = "https://youtube.com/playlist?list="
         self.reg = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
-        # Official API Client (cache_discovery=False to prevent warning error)
         self.youtube = (
             build("youtube", "v3", developerKey=YOUTUBE_API_KEY, cache_discovery=False)
             if YOUTUBE_API_KEY
@@ -151,49 +150,56 @@ class YouTubeAPI:
         else:
             vidid = self._extract_id(link)
 
-        if re.search(self.regex, link) or videoid:
-            req = self.youtube.videos().list(part="snippet,contentDetails", id=vidid)
+        if not self.youtube:
+            return "Unknown Title", "0:00", 0, "", ""
+
+        try:
+            if re.search(self.regex, link) or videoid:
+                req = self.youtube.videos().list(part="snippet,contentDetails", id=vidid)
+                res = req.execute()
+                if res.get("items"):
+                    item = res["items"][0]
+                    title = item["snippet"]["title"]
+                    thumbnail = item["snippet"]["thumbnails"]["high"]["url"]
+                    iso_dur = item["contentDetails"]["duration"]
+                    duration_sec = int(parse_duration(iso_dur).total_seconds())
+                    duration_min = seconds_to_min(duration_sec)
+                    return title, duration_min, duration_sec, thumbnail, vidid
+            
+            req = self.youtube.search().list(q=link, part="snippet", maxResults=1, type="video")
             res = req.execute()
             if res.get("items"):
                 item = res["items"][0]
+                vidid = item["id"]["videoId"]
                 title = item["snippet"]["title"]
                 thumbnail = item["snippet"]["thumbnails"]["high"]["url"]
-                iso_dur = item["contentDetails"]["duration"]
+                
+                v_req = self.youtube.videos().list(part="contentDetails", id=vidid)
+                v_res = v_req.execute()
+                iso_dur = v_res["items"][0]["contentDetails"]["duration"] if v_res.get("items") else "PT0S"
                 duration_sec = int(parse_duration(iso_dur).total_seconds())
                 duration_min = seconds_to_min(duration_sec)
                 return title, duration_min, duration_sec, thumbnail, vidid
-        
-        req = self.youtube.search().list(q=link, part="snippet", maxResults=1, type="video")
-        res = req.execute()
-        if res.get("items"):
-            item = res["items"][0]
-            vidid = item["id"]["videoId"]
-            title = item["snippet"]["title"]
-            thumbnail = item["snippet"]["thumbnails"]["high"]["url"]
-            
-            v_req = self.youtube.videos().list(part="contentDetails", id=vidid)
-            v_res = v_req.execute()
-            iso_dur = v_res["items"][0]["contentDetails"]["duration"] if v_res.get("items") else "PT0S"
-            duration_sec = int(parse_duration(iso_dur).total_seconds())
-            duration_min = seconds_to_min(duration_sec)
-            return title, duration_min, duration_sec, thumbnail, vidid
+        except Exception:
+            pass
 
-        return None, None, 0, None, None
+        return "Unknown Title", "0:00", 0, "", ""
 
     async def title(self, link: str, videoid: Union[bool, str] = None):
-        title, _, _, _, _ = await self.details(link, videoid)
-        return title
+        res = await self.details(link, videoid)
+        return res[0]
 
     async def duration(self, link: str, videoid: Union[bool, str] = None):
-        _, duration_min, _, _, _ = await self.details(link, videoid)
-        return duration_min
+        res = await self.details(link, videoid)
+        return res[1]
 
     async def thumbnail(self, link: str, videoid: Union[bool, str] = None):
-        _, _, _, thumbnail, _ = await self.details(link, videoid)
-        return thumbnail
+        res = await self.details(link, videoid)
+        return res[3]
 
     async def track(self, link: str, videoid: Union[bool, str] = None):
-        title, duration_min, _, thumbnail, vidid = await self.details(link, videoid)
+        res = await self.details(link, videoid)
+        title, duration_min, _, thumbnail, vidid = res
         track_details = {
             "title": title,
             "link": f"{self.base}{vidid}",
@@ -216,6 +222,8 @@ class YouTubeAPI:
 
     async def playlist(self, link, limit, user_id, videoid: Union[bool, str] = None):
         playlist_id = link.split("list=")[-1].split("&")[0] if "list=" in link else link
+        if not self.youtube:
+            return []
         try:
             req = self.youtube.playlistItems().list(
                 part="contentDetails",
@@ -256,20 +264,25 @@ class YouTubeAPI:
         return formats_available, link
 
     async def slider(self, link: str, query_type: int, videoid: Union[bool, str] = None):
-        req = self.youtube.search().list(q=link, part="snippet", maxResults=10, type="video")
-        res = req.execute()
-        items = res.get("items", [])
-        if len(items) > query_type:
-            item = items[query_type]
-            vidid = item["id"]["videoId"]
-            title = item["snippet"]["title"]
-            thumbnail = item["snippet"]["thumbnails"]["high"]["url"]
-            
-            v_req = self.youtube.videos().list(part="contentDetails", id=vidid)
-            v_res = v_req.execute()
-            iso_dur = v_res["items"][0]["contentDetails"]["duration"] if v_res.get("items") else "PT0S"
-            duration_min = seconds_to_min(int(parse_duration(iso_dur).total_seconds()))
-            return title, duration_min, thumbnail, vidid
+        if not self.youtube:
+            return None, None, None, None
+        try:
+            req = self.youtube.search().list(q=link, part="snippet", maxResults=10, type="video")
+            res = req.execute()
+            items = res.get("items", [])
+            if len(items) > query_type:
+                item = items[query_type]
+                vidid = item["id"]["videoId"]
+                title = item["snippet"]["title"]
+                thumbnail = item["snippet"]["thumbnails"]["high"]["url"]
+                
+                v_req = self.youtube.videos().list(part="contentDetails", id=vidid)
+                v_res = v_req.execute()
+                iso_dur = v_res["items"][0]["contentDetails"]["duration"] if v_res.get("items") else "PT0S"
+                duration_min = seconds_to_min(int(parse_duration(iso_dur).total_seconds()))
+                return title, duration_min, thumbnail, vidid
+        except Exception:
+            pass
         return None, None, None, None
 
     async def download(
@@ -297,4 +310,4 @@ class YouTubeAPI:
             return None, False
 
 YouTube = YouTubeAPI()
-        
+                        
